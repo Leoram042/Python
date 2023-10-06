122291a1f28603b722c2d0e2e48acb18dabc600a
Leoram042 <leoram042@gmail.com>
2023-10-06 11:45:58 +0530

    Integer Division and Float Divison

--- notes.py
+++ notes.py
@@ -3,3 +3,9 @@ Syntax: {variable_name:.<number_of_after_decimal_digits>f}
 """
 SAMPLE_FLOAT_VARIABLE = 10.123156
 print(f"Printing a float variable with 4 decimals using f-string:{SAMPLE_FLOAT_VARIABLE:.4f}")
+
+INT1 = 10
+INT2 = 2
+
+print(INT1//INT2) # Integer Division
+print(INT1/INT2) # Float Division
