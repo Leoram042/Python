90a47fb4ccc96987fe1d78c3a742fe5a83354f60
Leoram042 <leoram042@gmail.com>
2023-10-06 15:41:15 +0530

    Leap Year Logic

--- notes.py
+++ notes.py
@@ -7,5 +7,17 @@ print(f"Printing a float variable with 4 decimals using f-string:{SAMPLE_FLOAT_V
 INT1 = 10
 INT2 = 2
 
-print(INT1//INT2) # Integer Division
-print(INT1/INT2) # Float Division
+print(INT1 // INT2)  # Integer Division
+print(INT1 / INT2)  # Float Division
+
+
+LEAP_YEAR_TO_CHECK = 2000
+
+"""
+A year is a leap year if it is divisible by 400 or it is divisible by 4 and not by 100.
+"""
+
+LEAP_YEAR_STATUS = LEAP_YEAR_TO_CHECK % 400 == 0 or (
+    LEAP_YEAR_TO_CHECK % 4 == 0 and LEAP_YEAR_TO_CHECK % 100 != 0
+)
+print(LEAP_YEAR_STATUS)
