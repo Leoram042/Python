cf56d8bbaac7d48f086d5cf41f7ca051f377cc0b
Leoram042 <leoram042@gmail.com>
2023-10-06 11:32:43 +0530

    Printing Variable Floats of Decimal Values in f-strings

---
+++ notes.py
@@ -0,0 +1,5 @@
+"""
+Syntax: {variable_name:.<number_of_after_decimal_digits>f}
+"""
+SAMPLE_FLOAT_VARIABLE = 10.123156
+print(f"Printing a float variable with 4 decimals using f-string:{SAMPLE_FLOAT_VARIABLE:.4f}")
